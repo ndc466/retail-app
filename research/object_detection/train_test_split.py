792502e24943d1f43239905fdec100281694607b
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-05 16:20:50 -0600

    update

--- research/object_detection/train_test_split.py
+++ research/object_detection/train_test_split.py
@@ -80,6 +80,12 @@ def main():
     res = object_storage.put_object(namespace, 'training', 'ssd_mobilenet_v1_coco.config', coco)
     res = object_storage.put_object(namespace, 'training', 'row_labels.json', row_labels)
 
+    print('Writing config files to "data/" and "models/model" directories, respectively ...')
+    with open('data/object_detection.pbtxt', 'wb') as f:
+        f.write(pbtxt)
+    with open('models/model/ssd_mobilenet_v1_coco.config', 'wb') as f:
+        f.write(coco)
+
     # Write the corresponding image files to the Train and Test buckets
     print('Writing %s objects to "train_images" bucket ...' % (len(train)))
     for img_file in train['filename']:
