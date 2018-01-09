339c6dc4fcc421e0a840b1d996afaaac4743c3cd
Nolan Corcoran <nd.corc@gmail.com>
2018-01-09 22:05:36 +0000

    asdf

---
+++ chatbot/ochat-widget/templates/src/chat/choices.template.thml.ts
@@ -0,0 +1,5 @@
+export const choicesTemplate = ''+
+'<div class="ochat-card">'+
+'    <div class="ochat-choices-list"></div>'+
+'</div>'+
+'';
---
+++ chatbot/ochat-widget/templates/src/chat/message-wrapper.template.thml.ts
@@ -0,0 +1,9 @@
+export const messageWrapperTemplate = ''+
+'<div>'+
+'    <div class="ochat-message-wrapper">'+
+'        {message}'+
+'        <div class="ochat-cf"></div>'+
+'    </div>'+
+'    {choices}'+
+'</div>'+
+'';
