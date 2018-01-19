d5b66678b69ae17a233f8cb356f055f54ac3509b
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-19 13:34:34 -0600

    removed input parameters from export_inference_graph.py

--- research/object_detection/export_inference_graph.py
+++ research/object_detection/export_inference_graph.py
@@ -72,8 +72,9 @@ from object_detection import exporter
 from object_detection.protos import pipeline_pb2
 
 slim = tf.contrib.slim
-flags = tf.app.flags
 
+"""
+flags = tf.app.flags
 flags.DEFINE_string('input_type', 'image_tensor', 'Type of input node. Can be '
                     'one of [`image_tensor`, `encoded_image_string_tensor`, '
                     '`tf_example`]')
@@ -84,23 +85,36 @@ flags.DEFINE_string('trained_checkpoint_prefix', None,
                     'Path to trained checkpoint, typically of the form '
                     'path/to/model.ckpt')
 flags.DEFINE_string('output_directory', None, 'Path to write outputs.')
-
 FLAGS = flags.FLAGS
+"""
 
+def get_ckpt():
+    latest = []
+    files = os.listdir('./models/model/train')
+    for f in files:
+        if f.startswith('model.ckpt-'):
+            if latest == []:
+                latest = int(f[11:17])
+            else:
+                if int(f[11:17]) > latest: latest = int(f[11:17])
+    return str(latest)
 
 def main(_):
-  assert FLAGS.pipeline_config_path, '`pipeline_config_path` is missing'
-  assert FLAGS.trained_checkpoint_prefix, (
-         '`trained_checkpoint_prefix` is missing')
-  assert FLAGS.output_directory, '`output_directory` is missing'
+    """
+    assert FLAGS.pipeline_config_path, '`pipeline_config_path` is missing'
+    assert FLAGS.trained_checkpoint_prefix, ('`trained_checkpoint_prefix` is missing')
+    assert FLAGS.output_directory, '`output_directory` is missing'
+    """
 
-  pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
-  with tf.gfile.GFile(FLAGS.pipeline_config_path, 'r') as f:
-    text_format.Merge(f.read(), pipeline_config)
-  exporter.export_inference_graph(
-      FLAGS.input_type, pipeline_config, FLAGS.trained_checkpoint_prefix,
-      FLAGS.output_directory)
+    input_type = 'image_tensor'
+    ckpt = 'models/model/train/model.ckpt-' + get_ckpt()
+    if os.path.exists('output_graph') == True: shutil.rmtree('output_graph')
+    output_dir = 'output_graph'
 
+    pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+    with tf.gfile.GFile('models/model/ssd_mobilenet_v1_coco.config', 'r') as f:
+        text_format.Merge(f.read(), pipeline_config)
+    exporter.export_inference_graph('image_tensor', pipeline_config, ckpt, output_dir)
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
