53d628af1aa44c79951f959be75722309ea974dc
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-22 12:44:43 -0600

    asdf

--- object_detection/server.py
+++ object_detection/server.py
@@ -31,7 +31,7 @@ app.debug = DEBUG
 # What model to download.
 MODEL_NAME = 'inference_graphs'
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_CKPT = MODEL_NAME + '../../frozen_inference_graph.pb'
 # List of the strings that is used to add correct label for each box.
 PATH_TO_LABELS = os.path.join('training', 'object_detection.pbtxt')
 NUM_CLASSES = 3
