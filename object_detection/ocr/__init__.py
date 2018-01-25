2b5e56ceb1221f7e8f17a5f81ef64dadd5a2a0e0
Nolan Corcoran <nolan@identity.utexas.edu>
2018-01-25 14:05:14 -0600

    asdf

---
+++ object_detection/ocr/__init__.py
@@ -0,0 +1,43 @@
+from PIL import Image
+import pyocr
+import pyocr.builders
+import cv2
+import os
+import shutil
+
+tools = pyocr.get_available_tools()
+if len(tools) == 0:
+    print("No OCR tool found")
+    sys.exit(1)
+# The tools are returned in the recommended order of usage
+tool = tools[0]
+
+langs = tool.get_available_languages()
+lang = langs[0]
+
+def img_to_str(path):
+    img_type = path[:-3]
+    img = cv2.imread(path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    temp = 'temp'+img[-4:]
+    cv2.imwrite(temp)
+    img = Image.open(temp)
+
+    txt = tool.image_to_string(
+        img,
+        lang=lang,
+        builder=pyocr.builders.TextBuilder()
+    )
+
+    word_boxes = tool.image_to_string(
+        img,
+        lang="eng",
+        builder=pyocr.builders.WordBoxBuilder()
+    )
+
+    line_and_word_boxes = tool.image_to_string(
+        Image.open('test.png'), 
+        lang="eng",
+        builder=pyocr.builders.LineBoxBuilder()
+    )
+    return txt, word_boxes, line_and_word_boxes
