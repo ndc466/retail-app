fb5c7cef8eeb215f4df2fd37734d9cbd0b78e996
Nolan Corcoran <nolan@identity.utexas.edu>
2018-02-07 17:21:27 -0600

    asdf

--- object_detection/retrain.sh
+++ object_detection/retrain.sh
@@ -6,11 +6,9 @@ printf "\n\nGenerating tfrecord files from train and test sets\n"
 printf "________________________________________________\n\n"
 python generate_tfrecord.py
 
-printf "\n\nTraining/evaluating model on train/test tfrecords\n"
+printf "\n\nTraining model on train/test tfrecords\n"
 printf "________________________________________________\n\n"
-python train.py --logtostderr --train_dir=models/model/train --pipeline_config_path=models/model/ssd_mobilenet_v1_coco.config &
-sleep 60; python eval.py --logtostderr --checkpoint_dir=models/model/train/ --eval_dir=models/model/eval/ --pipeline_config_path=models/model/ssd_mobilenet_v1_coco.config &
-wait
+python train.py --logtostderr --train_dir=models/model/train --pipeline_config_path=models/model/ssd_mobilenet_v1_coco.config
 
 printf "\n\nTraining complete, exporting inference graph\n"
 printf "________________________________________________\n\n"
