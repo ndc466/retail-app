8f32f27f274f8276abf102c0e39b559067273ef0
Nolan Corcoran <nolan@identity.utexas.edu>
2018-03-05 19:17:58 -0600

    asdf

--- tf_detection/products_api.py
+++ tf_detection/products_api.py
@@ -8,10 +8,13 @@ from PIL import Image
 sys.path.append("..")
 from utils import label_map_util
 
-PATH_TO_CKPT = './output_graph/frozen_inference_graph.pb'
-PATH_TO_LABELS = './data/object_detection.pbtxt'
+#PATH_TO_CKPT = './output_graph/frozen_inference_graph.pb'
+#PATH_TO_LABELS = './data/object_detection.pbtxt'
+PATH_TO_CKPT = '../../output_graph/frozen_inference_graph.pb'
+PATH_TO_LABELS = '../../data/object_detection.pbtxt'
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 NUM_CLASSES = len(label_map.ListFields()[0][1])
+print(NUM_CLASSES)
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # Load a (frozen) Tensorflow model into memory.
